055fa823f6906c7bc288a6f4dd65007bc8aeec15
MartianovAV <andrei.martianoff92@yandex.ru>
2018-03-25 11:11:06 +0300

    //

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,25 @@
-/**
- * Created by anton on 24.01.18.
- */
-public class Main {
-    public static void main(String[] args) {
+class Calc{
+    public static int add(int x, int y){
+        return x + y;
+    }
+    public static int sub(int x, int y){
+        return x - y;
+    }
+    public static int mul(int x, int y){
+        return x * y;
+    }
+    public static int div(int x, int y){
+        return x / y;
+    }
+}
 
+
+public class Main {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 6;
+        int z;
+        z = Calc.add(x, y);
+        System.out.print("Сумма " + x + " и " + y + " равна " + z);
     }
 }
