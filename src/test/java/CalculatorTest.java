60a8ba772db03a9add2d504a5a777194c1d4889b
roninRonya <veronika-0-00@mail.ru>
2018-03-27 19:30:40 +0300

    New changes

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -26,5 +26,10 @@ public class CalculatorTest {
         assertEquals(1,Calculator.div(2,2));
 
     }
+    
+    @org.junit.Test
+    public void div() throws Exception {
+        assertEquals(1,Calculator.div(2,2));
 
+    }
 }
