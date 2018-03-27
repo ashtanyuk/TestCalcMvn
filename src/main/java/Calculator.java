ae39b09105b1f9ee5844f22dea7f80a7ccf1ac9c
svognev <33254651+svognev@users.noreply.github.com>
2018-03-27 19:21:06 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,7 @@
  */
 public class Calculator {
     public static int add(int x,int y) {
-        return x+y;
+        return x-y;
     }
     public static int sub(int x,int y) {
         return x+y;
