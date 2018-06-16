bd9b2d64b124a4fa827eb803d9e32b9254c1ea68
Алексей Рамаев <39632911+onekzy@users.noreply.github.com>
2018-06-16 13:39:25 +0300

    Update CalculatorTest.java
    add new test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -26,5 +26,9 @@ public class CalculatorTest {
         assertEquals(1,Calculator.div(2,2));
 
     }
+    
+    @org.junit.Test
+    public void divZero(timeout=10) throws Exception {
+       Calculator.div(2,2);
 
 }
